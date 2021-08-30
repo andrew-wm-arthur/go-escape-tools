4c8469f37a44ae2d5a71f4a67f08c59396433d26
GitHub Action <action@github.com>
2021-08-30 20:51:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/andrewwmarthur/goescapetools/MyBundle.kt
+++ src/main/kotlin/com/github/andrewwmarthur/goescapetools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.andrewwmarthur.goescapetools
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/andrewwmarthur/goescapetools/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.andrewwmarthur.goescapetools.services
+
+import com.github.andrewwmarthur.goescapetools.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
